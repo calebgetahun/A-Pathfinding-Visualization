a79f8669956d5c07c79bc7f6fde557bbaeb9fd70
czg3 <czg3@duke.edu>
2020-07-17 18:52:19 -0400

    setup before writing algorithm

--- astar.py
+++ astar.py
@@ -4,7 +4,7 @@ from queue import PriorityQueue
 
 WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("A* Path Finding Algorith")
+pygame.display.set_caption("A* Path Finding Algorithm")
 
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -47,7 +47,10 @@ class Node:
         return self.color == TURQUOISE
     
     def reset(self):
-        self.color == WHITE
+        self.color = WHITE
+
+    def make_start(self):
+        self.color = ORANGE
     
     def make_closed(self):
         self.color = RED
@@ -68,7 +71,18 @@ class Node:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row+1][self.col].is_barrier():  #Down
+            self.neighbors.append(grid[self.row+1][self.col])
+
+        if self.row > 0 and not grid[self.row-1][self.col].is_barrier():  #Up
+            self.neighbors.append(grid[self.row-1][self.col])
+
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col+1].is_barrier(): #Right
+            self.neighbors.append(grid[self.row][self.col+1])
+
+        if self.col > 0 and not grid[self.row][self.col-1].is_barrier(): #Left
+            self.neighbors.append(grid[self.row][self.col-1])
 
     def __lt__(self, other):
         return False
@@ -91,3 +105,85 @@ def make_grid(rows, width):
 
     return grid
 
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i*gap), (width, i*gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j*gap, 0), (j*gap, width))
+
+
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw(win)
+    
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+#helper function to get location of mouse click
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+    return row, col
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+
+    run = True
+    started = False
+
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if started:
+                continue
+            
+            if pygame.mouse.get_pressed()[0]: #LEFT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                if not start and node != end:
+                    start = node
+                    start.make_start()
+                
+                elif not end and node != start:
+                    end = node
+                    end.make_end()
+                
+                elif node != end and node != start:
+                    node.make_barrier()
+            elif pygame.mouse.get_pressed()[2]: #RIGHT
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors()
+
+                    algorithm(lambda : draw(win, grid, rows, width), grid, start, end)
+
+    pygame.quit()
+
+main(WIN, WIDTH)
