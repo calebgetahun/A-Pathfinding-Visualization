a9653355be53cfa7f99f65b2f97270ed82517be6
czg3 <czg3@duke.edu>
2020-07-17 22:07:22 -0400

    added A star algorithm to visualization

--- astar.py
+++ astar.py
@@ -87,12 +87,54 @@ class Node:
     def __lt__(self, other):
         return False
 
-
 def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
+def algorithm(draw, grid, start, end):
+    count = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count, start))
+    came_from = {}
+    g_score = {node: float("inf") for row in grid for node in row}
+    g_score[start] = 0
+    f_score = {node: float("inf") for row in grid for node in row}
+    f_score[start] = h(start.get_pos(), end.get_pos())
+
+    open_set_hash = {start}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+        
+        if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
+            return True
+        
+        for neighbor in current.neighbors:
+            temp_g_score = g_score[current] + 1
+
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
+                if neighbor not in open_set_hash:
+                    count+=1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.make_open()
+
+        draw()
+        if current != start:
+            current.make_closed()
+
+    return False
 
 def make_grid(rows, width):
     grid = []
@@ -141,16 +183,12 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
 
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            
-            if started:
-                continue
             
             if pygame.mouse.get_pressed()[0]: #LEFT
                 pos = pygame.mouse.get_pos()
@@ -166,6 +204,7 @@ def main(win, width):
                 
                 elif node != end and node != start:
                     node.make_barrier()
+
             elif pygame.mouse.get_pressed()[2]: #RIGHT
                 pos = pygame.mouse.get_pos()
                 row, col = get_clicked_pos(pos, ROWS, width)
@@ -177,12 +216,16 @@ def main(win, width):
                     end = None
             
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
-                            node.update_neighbors()
-
-                    algorithm(lambda : draw(win, grid, rows, width), grid, start, end)
+                            node.update_neighbors(grid)
+                    algorithm(lambda : draw(win, grid, ROWS, width), grid, start, end)
+            
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
 
     pygame.quit()
 
